80823167a306639b4e05ea0c7dbee217724bc054
dhalladay <dave.halladay@outlook.com>
2022-01-16 18:33:27 -0700

    refactor code

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,35 +3,26 @@ const notes = require('../../db/db.json');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const path = require('path');
+const createNote = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
-  res.json(notes);
+  return res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
   console.info(`${req.method} request received to add a note`);
   req.body.id = uuidv4();
-  const note = req.body;
-  notes.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, '../../db/db.json'),
-    JSON.stringify(notes, null, 2)
-  );
-  console.info(note);
-  res.json(notes);
+  notes.push(req.body);
+  createNote(notes);
+  return res.json(notes);
 });
 
 router.delete('/notes/:id', (req, res) => {
   console.info(`${req.method} request received to delete a note`);
-  let filteredData = [];
   const params = [req.params.id];
-  fs.readFile(path.join(__dirname, '../../db/db.json'), (err, data) => {
-    if (err) throw err;
-    return filteredData = JSON.parse(data).filter(x => x.id != `${params}`)
-      .then(fs.writeFile(
-        path.join(__dirname, '../../db/db.json'),
-        JSON.stringify(filteredData, null, 2)));
-      });
-});
+  const filteredArray = notes.filter(x => x.id != `${params}`);
+  createNote(filteredArray);
+  return res.json(filteredArray);
+  });
 
 module.exports = router;
