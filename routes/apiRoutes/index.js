17466f2ab158109de0d997a75a134ef06eca96f8
dhalladay <dave.halladay@outlook.com>
2022-01-16 19:18:01 -0700

    add comments to explain code

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,3 +1,4 @@
+//declare dependencies
 const router = require('express').Router();
 const notes = require('../../db/db.json');
 const { v4: uuidv4 } = require('uuid');
@@ -5,22 +6,32 @@ const fs = require('fs');
 const path = require('path');
 const createNote = require('../../lib/notes');
 
+//GET route
 router.get('/notes', (req, res) => {
   return res.json(notes);
 });
 
+//POST route
 router.post('/notes', (req, res) => {
+  //advise that POST request was received
   console.info(`${req.method} request received to add a note`);
+  //create unique id using uuid npm package
   req.body.id = uuidv4();
+  //add new note to the bottom of db.json
   notes.push(req.body);
+  //rewrite the notes to db.json
   createNote(notes);
   return res.json(notes);
 });
 
+//DELETE route
 router.delete('/notes/:id', (req, res) => {
+  //Advise that delete request was received
   console.info(`${req.method} request received to delete a note`);
   const params = [req.params.id];
+  //user .filter() to create a new array without the deleted note
   const filteredArray = notes.filter(x => x.id != `${params}`);
+  //rewrite the notes to db.json
   createNote(filteredArray);
   return res.json(filteredArray);
   });
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,14 +2,17 @@ const path = require('path');
 const fs = require('fs');
 const router = require('express').Router();
 
+//get request for home page html
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
+// routing for notes page html
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
+//catch-all route to direct to home page
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
